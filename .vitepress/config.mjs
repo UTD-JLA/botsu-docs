b2bc727772472ad8639d9f1230654201b09f913d
Juan Llamas <38849891+xoltia@users.noreply.github.com>
2023-10-09 12:00:13 -0500

    Change base URL

--- .vitepress/config.mjs
+++ .vitepress/config.mjs
@@ -2,6 +2,7 @@ import { defineConfig } from 'vitepress'
 
 // https://vitepress.dev/reference/site-config
 export default defineConfig({
+  base: '/botsu-docs/',
   title: "Botsu",
   description: "Documentation for Botsu bot",
   themeConfig: {
